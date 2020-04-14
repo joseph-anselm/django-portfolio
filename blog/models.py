60df84e8ce6b274cf33795c0fa4412f782ea884c
Joe Anselm <iamjoeanselm@gmail.com>
2020-04-14 19:50:56 +0100

    mades changes

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
 class blog(models.Model):
     title=models.CharField(max_length=100)
     body=models.CharField(max_length=5000)
-    thumbnails=models.ImageField()
+    thumbnails=models.ImageField(upload_to='images', blank=True,)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import blog
 
 # Create your views here.
+def home(request):
+    blog = blog.objects.all()
+    return render(request, 'blog/home.html', {'blog':blog})
